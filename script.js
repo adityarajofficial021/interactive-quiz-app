9468f370dfcf6c36d1e70e2178589c86b422b58a
adityarajofficial021 <adityarajofficial021@gmail.com>
2026-06-15 21:17:29 +0530

    Add initial quiz question bank

---
+++ script.js
@@ -0,0 +1,36 @@
+const questions = [
+  {
+    question: "What does JS stand for?",
+    options: [
+      "JavaScript",
+      "Java Source",
+      "Just Script",
+      "Java System"
+    ],
+    answer: "JavaScript"
+  },
+
+  {
+    question: "Which keyword is used to declare a variable?",
+    options: [
+      "let",
+      "print",
+      "show",
+      "echo"
+    ],
+    answer: "let"
+  },
+
+  {
+    question: "Which company developed JavaScript?",
+    options: [
+      "Netscape",
+      "Microsoft",
+      "Google",
+      "Apple"
+    ],
+    answer: "Netscape"
+  }
+];
+
+console.log(questions);
